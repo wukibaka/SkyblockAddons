709856e128ca7fdd3420c54366aef4dabd96d0c9
wuki <51240031+wukibaka@users.noreply.github.com>
2023-02-26 16:36:45 +0800

    更新镜像

--- src/main/java/codes/biscuit/skyblockaddons/utils/data/DataConstants.java
+++ src/main/java/codes/biscuit/skyblockaddons/utils/data/DataConstants.java
@@ -1,7 +1,7 @@
 package codes.biscuit.skyblockaddons.utils.data;
 
 class DataConstants {
-    static final String CDN_BASE_URL = "https://ghproxy.com/https://raw.githubusercontent.com/BiscuitDevelopment/SkyblockAddons-Data/main/";
+    static final String CDN_BASE_URL = "https://github.wuki.moe/https://raw.githubusercontent.com/BiscuitDevelopment/SkyblockAddons-Data/main/";
     static final String FALLBACK_CDN_BASE_URL = "https://skyblockaddonsdata.onrender.com/";
     static final String GITHUB_BASE_URL = "https://raw.githubusercontent.com/BiscuitDevelopment/SkyblockAddons-Data/main/";
 }
